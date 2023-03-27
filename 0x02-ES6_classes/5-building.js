78e16b4ddaa7c09b1156a80909121f3264f02deb
Jamal <jayjamal86@gmail.com>
2023-03-27 14:56:08 +0300

    evacuation message error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(newSqft) {
-    if (typeof newSqft === 'number') {
-      this._sqft = newSqft;
-    } else {
-      throw new TypeError('Sqft must be a number');
-    }
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
