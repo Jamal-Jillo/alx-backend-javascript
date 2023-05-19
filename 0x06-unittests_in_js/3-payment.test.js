36f33ff3f987977f9d30ed5272d959ba13f76cbb
Jamal <jayjamal86@gmail.com>
2023-05-19 23:30:16 +0300

    3-4-5-6 basic tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,22 +1,15 @@
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    const totalAmount = 100;
-    const totalShipping = 20;
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-    sendPaymentRequestToApi(totalAmount, totalShipping);
-
-    sinon.assert.calledWithExactly(
-      calculateNumberSpy,
-      'SUM',
-      totalAmount,
-      totalShipping
-    );
-
-    calculateNumberSpy.restore();
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,37 +1,20 @@
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberStub;
-  let consoleLogSpy;
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-  beforeEach(() => {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    consoleLogSpy = sinon.spy(console, 'log');
-  });
-
-  afterEach(() => {
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    const totalAmount = 100;
-    const totalShipping = 20;
-
-    sendPaymentRequestToApi(totalAmount, totalShipping);
-
-    expect(calculateNumberStub.calledOnceWithExactly('SUM', totalAmount, totalShipping)).to.be.true;
-  });
-
-  it('should log the correct message', () => {
-    const totalAmount = 100;
-    const totalShipping = 20;
-
-    sendPaymentRequestToApi(totalAmount, totalShipping);
-
-    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
